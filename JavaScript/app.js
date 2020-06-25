cccf70c2db0704caf59c73a151ca59f16c075b99
AmimoG <gilbertmatete6@gmail.com>
2020-06-25 09:31:18 +0300

    I have commited the changes

---
+++ JavaScript/app.js
@@ -0,0 +1,15 @@
+// ************ set date **************
+const date = document.getElementById('date')
+date.innerHTML = new Date().getFullYear();
+// ************ close links ***********
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    
+})
+
+// ************ fixed navbar **********
+
+// ************ smooth scroll **********
