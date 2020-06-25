b3ddc83baf4477b56b4e67509b29e163353bbb7b
AmimoG <gilbertmatete6@gmail.com>
2020-06-25 14:56:03 +0300

    Commit

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,3 @@
 // ************ set date **************
 const date = document.getElementById('date')
 date.innerHTML = new Date().getFullYear();
-// ************ close links ***********
-const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = document.querySelector('.links-container');
-const links = document.querySelector('.links');
-
-navToggle.addEventListener('click', function(){
-    
-})
-
-// ************ fixed navbar **********
-
-// ************ smooth scroll **********
